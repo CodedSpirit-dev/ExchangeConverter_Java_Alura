2d440908d7043e5a445af200d17cd45dcd8bd4f5
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-05-18 13:16:08 -0600

    Merging files

--- src/exchangeconverter/controller/ExchangeRateResponse.java
+++
@@ -1,9 +0,0 @@
-package exchangeconverter.controller;
-
-public class ExchangeRateResponse {
-    private double conversionRate;
-
-    public double getConversionRate() {
-        return conversionRate;
-    }
-}
